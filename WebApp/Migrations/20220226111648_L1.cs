79c12e98a1835e43c3a9305171dffc3e5ce35f5e
Administrator <Administrator@CHETUIWK1764>
2022-03-01 15:10:41 +0530

    Add project files.

---
+++ WebApp/Migrations/20220226111648_L1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class L1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastNAme = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ WebApp/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Display(Name = "Student FirstName")]
+        [Required(ErrorMessage = "Student  FirstName can't Empty !")]
+        public string FirstName { get; set; }
+        [Display(Name = "Student LastName")]
+        [Required(ErrorMessage = "Student LastName can't Empty !")]
+        public string LastName { get; set; }
+        [Display(Name = "Student Email Address")]
+        [Required(ErrorMessage = "Student Email can't Empty !")]
+        [EmailAddress(ErrorMessage = "Email Address is not valid !")]
+        public string Email { get; set; }
+        [Display(Name = "Student Mobile")]
+        [Required(ErrorMessage = "Student Mobile can't Empty !")]
+        [RegularExpression(@"^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$",
+            ErrorMessage = "Invalid Mobile number !")]
+        public string Mobile { get; set; }
+        [Display(Name = "Student Gender")]
+        [Required(ErrorMessage = "Student Gender can't Empty !")]
+        public string Gender { get; set; }
+    }
+}
